417a43ff3db49732034c1bf8a161c20cb6de04c4
FiachraCI <fiachraholland@gmail.com>
2021-07-31 16:06:36 +0000

    Added API calls for Mythology section

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,35 @@
+// API Calls grouped by category
+
+// Mythology
+var data;
+
+async function myt_e() {
+  let response = await fetch(`https://opentdb.com/api.php?amount=1&category=20&difficulty=easy&type=multiple`);
+  console.log(response)
+  data = await response.json();
+  console.log(data)
+}
+
+async function myt_m() {
+  let response = await fetch(`https://opentdb.com/api.php?amount=1&category=20&difficulty=medium&type=multiple`);
+  console.log(response)
+  data = await response.json();
+  console.log(data)
+}
+
+async function myt_h() {
+  let response = await fetch(`https://opentdb.com/api.php?amount=1&category=20&difficulty=hard&type=multiple`);
+  console.log(response)
+  data = await response.json();
+  console.log(data)
+}
+
+// Quiz Boilerplate Function
+
 function populateBP2() {
   $('.quiz-box-container').html(
     `<div class="question-area">
-    <h3 class="mt-3">
-      "In which 1979 film was the spaceship called Nostromo?"
+    <h3 class="mt-3" id="question">
     </h3>
   </div>
   <div class="answer-area">
@@ -15,12 +42,14 @@ function populateBP2() {
           <p class="score results">
             Score:<span id="operand1">0</span>/<span id="operand2">0</span>
           </p>
-          <p class="time results">Time:<span id="operand3">x</span></p>
+          <p class="time results">Time:<span id="operand3"></span>:<span id="operand4"></span></p>
         </div>
       </div>
 </div>`
   );
 }
+
+// Quiz Initialization Message
 
 function populateBP1() {
   $('.quiz-box-container').html(
@@ -33,20 +62,52 @@ function populateBP1() {
   );
 }
 
+// Mythology Game Selection
+
 $('#mythology').click(function () {
   $('#easy')
     .removeClass()
     .addClass('difficulty2')
     .click(function () {
+      myt_e();
       populateBP1();
       setTimeout(function () {
-        populateBP2()
+        populateBP2();
       }, 5000);
+      setTimeout(function () {
+        runQuiz(data);
+      }, 5001);
     });
+});
+
+$('#mythology').click(function () {
   $('#medium')
     .removeClass()
-    .addClass('difficulty2');
+    .addClass('difficulty2')
+    .click(function () {
+      myt_m();
+      populateBP1();
+      setTimeout(function () {
+        populateBP2();
+      }, 5000);
+      setTimeout(function () {
+        runQuiz(data);
+      }, 5001);
+    });
+});
+
+$('#mythology').click(function () {
   $('#hard')
     .removeClass()
     .addClass('difficulty2')
+    .click(function () {
+      myt_h();
+      populateBP1();
+      setTimeout(function () {
+        populateBP2();
+      }, 5000);
+      setTimeout(function () {
+        runQuiz(data);
+      }, 5001);
+    });
 });
